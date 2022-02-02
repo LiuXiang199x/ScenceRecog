72b3f05594858279529282ef89f947d653fd1da0
marco <lx19961026@gmail.com>
2022-02-02 21:53:39 +0800

    2/2

--- 数据增强/noise.py
+++ 数据增强/noise.py
@@ -2,7 +2,51 @@
 import cv2 as cv
 import imutils
 import argparse
+import numpy as np
+import random
 
 origin_img_path = "./DEDUCE/data/val/bed_room/RGB100W_542500000083_116445085381751238.jpg"
 
+def sp_noise(image, prob):
+    '''
+    添加椒盐噪声
+    prob:噪声比例 
+    '''
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob 
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = image[i][j]
+    return output
+ 
+def gasuss_noise(image, mean=0, var=0.001):
+    ''' 
+    添加高斯噪声
+    mean : 均值 
+    var : 方差
+    '''
+    image = np.array(image/255, dtype=float)
+    noise = np.random.normal(mean, var ** 0.5, image.shape)
+    out = image + noise
+    if out.min() < 0:
+        low_clip = -1.
+    else:
+        low_clip = 0.
+    out = np.clip(out, low_clip, 1.0)
+    out = np.uint8(out*255)
+    #cv.imshow("gasuss", out)
+    return out
 
+img = cv.imread(origin_img_path)
+# a = sp_noise(img, 0.02)
+a = gasuss_noise(img)
+
+cv.imshow("111111:", a)
+cv.waitKey(0)
+print(a.shape)
