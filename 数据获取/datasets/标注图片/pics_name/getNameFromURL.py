0dc637f9319801c5ff62963de10010dfc6c38182
Marco <lx19961026@gmail.com>
2022-02-22 15:21:23 +0800

    allpics

--- 数据获取/datasets/标注图片/pics_name/getNameFromURL.py
+++ 数据获取/datasets/标注图片/pics_name/getNameFromURL.py
@@ -2,8 +2,8 @@ import requests
 import imghdr
 
 def ff():
-    names_file = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_name/diningtable/diningtable_filter.txt", "w")
-    url_file = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/diningtable/diningtable_filter.txt")
+    names_file = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_name/bed/bed_filter_others.txt", "w")
+    url_file = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/bed/bed_filter_others.txt")
     url_datas = url_file.readlines()
 
     for data_url in url_datas:
--- 数据获取/downloadpic_fromurls.py
+++ 数据获取/downloadpic_fromurls.py
@@ -27,7 +27,7 @@ def download_pics_withURL():
 # CRL + JPG
 
 
-def filer_pics_bed():
+def filer_pics_visulgood():
     url_file_f = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/diningtable/diningtable_filter.txt", "w")
     url_file = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/diningtable/diningtable.txt")
     url_datas = url_file.readlines()
@@ -48,4 +48,21 @@ def filer_pics_bed():
                  url_file_f.writelines(data+"\n")
                    
                     
-filer_pics_bed()
+def filer_pics_visulbad_notslam():
+    url_file_f = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/sofa/sofa_filter_others.txt", "w")
+    url_file = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/sofa/sofa.txt")
+    url_datas = url_file.readlines()
+
+    for data_url in url_datas:
+        data = data_url.replace("\n","")
+        data = data.replace("\t","")
+        if not "png" in data:            
+            if "rB" not in data:
+                if "PURCHASE" not in data:
+                    if "slam" not in data:
+                        if data[-6]!="_" and data[-7]!="_" and data[-8]!="_" and data[-9]!="_" and data[-10]!="_":
+                            if "UP" not in data:
+                                if "calibration" not in data:
+                                    url_file_f.writelines(data+"\n")
+                             
+filer_pics_visulbad_notslam()
