717dde006912e42296318d7dfa033f6e7b50edba
Marco <lx19961026@gmail.com>
2022-03-04 10:34:41 +0800

    balcony_kitchen

--- 数据获取/datasets/标注图片/pics_url/seperate_url_names.py
+++ 数据获取/datasets/标注图片/pics_url/seperate_url_names.py
@@ -1,7 +1,7 @@
 import os
 
-f1 = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/doorConor_hxy/场景分类_wallConor_hxy.txt").readlines()
-f2 = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/doorConor_hxy/场景分类_wallConor_hxy_urls.txt", "w")
+f1 = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/kitchen_hxy/场景分类_kitchen_hxy(original).txt").readlines()
+f2 = open("/home/agent/ScenceRecog/数据获取/datasets/标注图片/pics_url/kitchen_hxy/场景分类_kitchen_hxy.txt", "w")
 for item in f1:
     tmp_url = item.split("\t")[0]
     
